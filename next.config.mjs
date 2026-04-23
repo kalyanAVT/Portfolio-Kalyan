25613eb2fc463df722ff183005eeba29c5f0b360
kalyanAVT <allulakalyan3@gmail.com>
2026-04-23 15:58:51 +0530

    chore: disable Next.js development indicators and overlays

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      }
-    ],
+  devIndicators: {
+    staticIndicator: false,
+    buildActivity: false,
+    appIsrStatus: false,
   },
 };
-
 export default nextConfig;
